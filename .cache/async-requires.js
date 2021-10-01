9504c15d8d778326ccd21f93784d009f22e0e91d
jdanigo <tatanbmx@gmail.com>
2021-10-01 12:13:53 -0500

    commit html

---
+++ .cache/async-requires.js
@@ -0,0 +1,13 @@
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+exports.components = {
+  "component---cache-caches-gatsby-plugin-offline-app-shell-js": () => import("./../../caches/gatsby-plugin-offline/app-shell.js" /* webpackChunkName: "component---cache-caches-gatsby-plugin-offline-app-shell-js" */),
+  "component---src-pages-404-index-jsx": () => import("./../../../src/pages/404/index.jsx" /* webpackChunkName: "component---src-pages-404-index-jsx" */),
+  "component---src-pages-blog-index-jsx": () => import("./../../../src/pages/blog/index.jsx" /* webpackChunkName: "component---src-pages-blog-index-jsx" */),
+  "component---src-pages-contact-index-jsx": () => import("./../../../src/pages/contact/index.jsx" /* webpackChunkName: "component---src-pages-contact-index-jsx" */),
+  "component---src-pages-index-jsx": () => import("./../../../src/pages/index.jsx" /* webpackChunkName: "component---src-pages-index-jsx" */),
+  "component---src-pages-resume-jsx": () => import("./../../../src/pages/resume.jsx" /* webpackChunkName: "component---src-pages-resume-jsx" */),
+  "component---src-pages-tags-index-jsx": () => import("./../../../src/pages/tags/index.jsx" /* webpackChunkName: "component---src-pages-tags-index-jsx" */)
+}
+
---
+++ .cache/sync-requires.js
@@ -0,0 +1,16 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-caches-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/Users/daniel/Documents/GitHub/jdanigo.github.io/.cache/caches/gatsby-plugin-offline/app-shell.js"))),
+  "component---src-pages-404-index-jsx": hot(preferDefault(require("/Users/daniel/Documents/GitHub/jdanigo.github.io/src/pages/404/index.jsx"))),
+  "component---src-pages-blog-index-jsx": hot(preferDefault(require("/Users/daniel/Documents/GitHub/jdanigo.github.io/src/pages/blog/index.jsx"))),
+  "component---src-pages-contact-index-jsx": hot(preferDefault(require("/Users/daniel/Documents/GitHub/jdanigo.github.io/src/pages/contact/index.jsx"))),
+  "component---src-pages-index-jsx": hot(preferDefault(require("/Users/daniel/Documents/GitHub/jdanigo.github.io/src/pages/index.jsx"))),
+  "component---src-pages-resume-jsx": hot(preferDefault(require("/Users/daniel/Documents/GitHub/jdanigo.github.io/src/pages/resume.jsx"))),
+  "component---src-pages-tags-index-jsx": hot(preferDefault(require("/Users/daniel/Documents/GitHub/jdanigo.github.io/src/pages/tags/index.jsx")))
+}
+
